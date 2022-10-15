0ed9d3d67d55f141e840f05d723d9041a4fc4680
5HT2 <git@frogg.ie>
2022-10-14 21:24:55 -0700

    impr: Separate `api` into package, add more documentation

---
+++ api/api.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"fmt"
+	"github.com/effectindex/tripreporter/ui"
+	"github.com/effectindex/tripreporter/util"
+	"io/fs"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+)
+
+var (
+	proxy *httputil.ReverseProxy // proxy is used for `make dev-ui`
+	dev   = false                // will disable serving /static/ from cache and proxy un-handled requests
+)
+
+// Setup manages functions that should be ready to use before
+func Setup(isDevelopment bool) {
+	proxy = util.NewProxy("http://localhost:" + os.Getenv("DEV_PORT"))
+	dev = isDevelopment
+}
+
+// Handler manages all paths, with Router handling anything not defined here.
+func Handler() http.Handler {
+	mux := http.NewServeMux()
+
+	// let api.Router do everything else
+	mux.HandleFunc("/", Router)
+
+	// serve /static/ by cache in production (no hot-reload support)
+	if !dev { // if running in development mode, let api.Router reverse proxy it
+		staticFS, _ := fs.Sub(ui.StaticFiles, "dist")
+		httpFS := http.FileServer(http.FS(staticFS))
+		mux.Handle("/static/", httpFS)
+	}
+
+	// API functions
+	mux.HandleFunc("/api/v1/greeting", greetingAPI)
+	return mux
+}
+
+// Router will route everything except /static/ and valid /api/ endpoints.
+func Router(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, http.StatusText(http.StatusMethodNotAllowed))
+		return
+	}
+
+	// For API endpoints not already handled in Handler()
+	if strings.HasPrefix(r.URL.Path, "/api") {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.URL.Path == "/favicon.ico" {
+		rawFile, _ := ui.StaticFiles.ReadFile("dist/favicon.ico")
+		w.Write(rawFile)
+		return
+	}
+
+	if dev { // serve everything else in dev to the proxy, if in dev
+		proxy.ServeHTTP(w, r)
+	} else { // else, serve just dist/index.html and let Vue's JS handle it
+		rawFile, _ := ui.StaticFiles.ReadFile("dist/index.html")
+		w.Write(rawFile)
+	}
+}
+
+func greetingAPI(w http.ResponseWriter, r *http.Request) { // TODO: Remove / use individual files for routes
+	w.Write([]byte("Hello, there!"))
+}
--- main.go
+++ main.go
@@ -3,22 +3,17 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
-	"github.com/effectindex/tripreporter/ui"
-	"github.com/effectindex/tripreporter/util"
+	"github.com/effectindex/tripreporter/api"
 	"github.com/joho/godotenv"
-	"io/fs"
 	"log"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"strings"
 	"time"
 )
 
 var (
-	dev   = flag.Bool("dev", false, "Run in development mode, alongside `make dev-ui`.")
-	proxy *httputil.ReverseProxy
+	dev = flag.Bool("dev", false, "Run in development mode, alongside `make dev-ui`.")
 )
 
 func main() {
@@ -35,10 +30,10 @@ func main() {
 	}
 
 	// Setup proxy to webpack hot-reload server (for dev-ui) and regular http server (serves everything)
-	proxy = util.NewProxy("http://localhost:" + os.Getenv("DEV_PORT")) // proxy is used for `make dev-ui`
+	api.Setup(*dev)
 	s := &http.Server{
 		Addr:        "localhost:" + os.Getenv("SRV_PORT"),
-		Handler:     Handler(),
+		Handler:     api.Handler(),
 		IdleTimeout: time.Minute,
 	}
 
@@ -51,56 +46,6 @@ func main() {
 	if err := s.ListenAndServe(); err != nil {
 		log.Printf("error in ListenAndServe: %v\n", err)
 	}
-}
-
-func Handler() http.Handler {
-	mux := http.NewServeMux()
-
-	// let Router do everything else
-	mux.HandleFunc("/", Router)
-
-	// serve /static/ by cache in production (no hot-reload support)
-	if !*dev { // if running in development mode, let Router reverse proxy it
-		staticFS, _ := fs.Sub(ui.StaticFiles, "dist")
-		httpFS := http.FileServer(http.FS(staticFS))
-		mux.Handle("/static/", httpFS)
-	}
-
-	// API functions
-	mux.HandleFunc("/api/v1/greeting", greetingAPI)
-	return mux
-}
-
-// Router will route everything except /static/ and valid /api/ endpoints.
-func Router(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, http.StatusText(http.StatusMethodNotAllowed))
-		return
-	}
-
-	// For API endpoints not already handled in Handler()
-	if strings.HasPrefix(r.URL.Path, "/api") {
-		http.NotFound(w, r)
-		return
-	}
-
-	if r.URL.Path == "/favicon.ico" {
-		rawFile, _ := ui.StaticFiles.ReadFile("dist/favicon.ico")
-		w.Write(rawFile)
-		return
-	}
-
-	if *dev {
-		proxy.ServeHTTP(w, r)
-	} else {
-		rawFile, _ := ui.StaticFiles.ReadFile("dist/index.html")
-		w.Write(rawFile)
-	}
-}
-
-func greetingAPI(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, there!"))
 }
 
 func validateEnvKeys(keys ...string) error {
