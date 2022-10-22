0c6753a05301731e7dd6145a64664df1ba20b14f
5HT2 <git@frogg.ie>
2022-10-23 00:54:42 +0100

    impr: Allow non-localhost, and check `SITE_NAME` is set

--- main.go
+++ main.go
@@ -25,14 +25,15 @@ func main() {
 		log.Fatalf("err loading .env file (copy the .env.example): %v\n", err)
 	}
 
-	if err := validateEnvKeys("DEV_PORT", "SRV_PORT"); err != nil {
+	// "ADDRESS" can be empty, it's the only optional one
+	if err := validateEnvKeys("DEV_PORT", "SRV_PORT", "SITE_NAME"); err != nil {
 		log.Fatalf("missing .env variables (copy the .env.example): %v\n", err)
 	}
 
 	// Setup proxy to webpack hot-reload server (for dev-ui) and regular http server (serves everything)
 	api.Setup(*dev)
 	s := &http.Server{
-		Addr:        "localhost:" + os.Getenv("SRV_PORT"),
+		Addr:        os.Getenv("ADDRESS") + ":" + os.Getenv("SRV_PORT"),
 		Handler:     api.Handler(),
 		IdleTimeout: time.Minute,
 	}
