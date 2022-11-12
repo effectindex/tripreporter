fbf1272cd5f15fb96c765632fb9cc36d59891cc8
5HT2 <git@frogg.ie>
2022-11-11 17:36:11 -0800

    impr: Implement Context model

---
+++ models/context.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"go.uber.org/zap"
+)
+
+type Context struct {
+	Database *pgxpool.Pool
+	Logger   *zap.SugaredLogger
+}
+
+func (ctx *Context) DB() pgx.Tx {
+	ctx.Validate()
+
+	db, err := ctx.Database.Begin(context.Background())
+	if err != nil {
+		ctx.Logger.Errorw("Failed to begin db transaction", zap.Error(err))
+		return nil
+	}
+
+	return db
+}
+
+func (ctx *Context) Validate() {
+	if ctx.Logger == nil {
+		panic(ErrorContextNilLogger)
+	}
+
+	if ctx.Database == nil {
+		panic(ErrorContextNilDatabase)
+	}
+}
