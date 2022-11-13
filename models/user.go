4e7fef7607b0e75b4ce58eae1a09bc9850f53f65
5HT2 <git@frogg.ie>
2022-11-12 20:16:34 -0800

    impr: Add available db tags to User model

--- models/user.go
+++ models/user.go
@@ -7,14 +7,14 @@ import (
 type User struct { // todo: old name was Profile // todo: visible on public profile:
 	Context
 	Type        string          `json:"type"`
-	Created     Time            `json:"created"`
-	DisplayName string          `json:"display_name"`  // Optional
-	Birth       Time            `json:"date_of_birth"` // Optional, date in UTC+0, use Age if unset
-	Age         int64           `json:"age"`           // Optional, updated by Birth and unfavored if Age set
-	Height      decimal.Decimal `json:"height"`        // Optional // TODO: Encryption?
-	Weight      decimal.Decimal `json:"weight"`        // Optional // TODO: Encryption?
-	Medication  UserMedication  `json:"medication"`    // User's saved medication // TODO: Add to schema
-	Preferences UserPreferences `json:"preferences"`   // User's preferences // TODO: Add to schema
+	Created     Time            `json:"created" db:"created"`
+	DisplayName string          `json:"display_name" db:"display_name"`   // Optional
+	Birth       Time            `json:"date_of_birth" db:"date_of_birth"` // Optional, date in UTC+0, use Age if unset
+	Age         int64           `json:"age" db:"age"`                     // Optional, updated by Birth and unfavored if Age set
+	Height      decimal.Decimal `json:"height" db:"height"`               // Optional // TODO: Encryption?
+	Weight      decimal.Decimal `json:"weight" db:"weight"`               // Optional // TODO: Encryption?
+	Medication  UserMedication  `json:"medication"`                       // User's saved medication // TODO: Add to schema
+	Preferences UserPreferences `json:"preferences"`                      // User's preferences // TODO: Add to schema
 }
 
 func (a *User) Get() error {
