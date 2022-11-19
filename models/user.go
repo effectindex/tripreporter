7ea6fdea935c41c153cb7c66dd33d4360c71891d
5HT2 <git@frogg.ie>
2022-11-19 12:54:07 -0800

    impr: Properly support `User.Age` and `User.Birth`, implement `Patch()`, fix `Post()`

--- models/user.go
+++ models/user.go
@@ -3,21 +3,21 @@ package models
 import (
 	"context"
 	"github.com/georgysavva/scany/v2/pgxscan"
-
-	"github.com/shopspring/decimal"
 	"go.uber.org/zap"
+	"strconv"
+	"strings"
 )
 
 type User struct { // todo: old name was Profile // todo: visible on public profile:
 	Context
 	Unique
 	Type        string          `json:"type"`
-	Created     Time            `json:"created" db:"created"`
+	Created     Timestamp       `json:"created" db:"created"`             // Required, set by default.
 	DisplayName string          `json:"display_name" db:"display_name"`   // Optional
-	Birth       Time            `json:"date_of_birth" db:"date_of_birth"` // Optional, date in UTC+0, use Age if unset
-	Age         int64           `json:"age" db:"age"`                     // Optional, updated by Birth and unfavored if Age set
-	Height      decimal.Decimal `json:"height" db:"height"`               // Optional // TODO: Encryption?
-	Weight      decimal.Decimal `json:"weight" db:"weight"`               // Optional // TODO: Encryption?
+	Birth       Timestamp       `json:"date_of_birth" db:"date_of_birth"` // Optional, use Age if unset
+	Age         Age             `json:"age"`                              // Optional, updated by Birth and unfavored if Age set
+	Height      Decimal         `json:"height" db:"height"`               // Optional // TODO: Encryption?
+	Weight      Decimal         `json:"weight" db:"weight"`               // Optional // TODO: Encryption?
 	Medication  UserMedication  `json:"medication"`                       // User's saved medication // TODO: Add to schema
 	Preferences UserPreferences `json:"preferences"`                      // User's preferences // TODO: Add to schema
 }
@@ -32,7 +32,7 @@ func (u *User) Get() (*User, error) {
 
 	var u1 []*User
 	if err := pgxscan.Select(context.Background(), db, &u1,
-		`select created, display_name, date_of_birth, age, height, weight from users where account_id = $1;`, u.ID,
+		`select created, display_name, date_of_birth, height, weight from users where account_id = $1;`, u.ID,
 	); err != nil {
 		u.Logger.Warnw("Failed to get user from DB", zap.Error(err))
 		return u, err
@@ -45,9 +45,12 @@ func (u *User) Get() (*User, error) {
 		u.Created = u1[0].Created
 		u.DisplayName = u1[0].DisplayName
 		u.Birth = u1[0].Birth
-		u.Age = u1[0].Age
 		u.Height = u1[0].Height
 		u.Weight = u1[0].Weight
+
+		if u.Birth.Valid() {
+			u.Age.Update(u.Birth)
+		}
 	}
 
 	return u, nil
@@ -61,14 +64,18 @@ func (u *User) Post() (*User, error) {
 		return u, ErrorUserNotSpecified
 	}
 
-	if !u.Created.Set() {
-		u.Created.New()
+	if !u.Created.Valid() {
+		u.Created.Now()
+	}
+
+	if u.Birth.Valid() {
+		u.Age.Update(u.Birth)
 	}
 
 	if _, err := db.Exec(context.Background(),
-		`insert into users(account_id, created, display_name, date_of_birth, age, height, weight)
-		values($1, $2, $3, $4, $5, $6, $7);`,
-		u.ID, u.Created.String(), u.DisplayName, u.Birth.String(), u.Age, u.Height, u.Weight, // TODO: Medication / preferences in DB?
+		`insert into users(account_id, created, display_name, date_of_birth, height, weight)
+		values($1, $2, $3, $4, $5, $6);`,
+		u.ID, u.Created.String(), u.DisplayName, u.Birth.String(), u.Height.String(), u.Weight.String(), // TODO: Medication / preferences in DB?
 	); err != nil {
 		u.Logger.Warnw("Failed to write account to DB", zap.Error(err))
 		_ = db.Rollback(context.Background())
@@ -79,7 +86,67 @@ func (u *User) Post() (*User, error) {
 }
 
 func (u *User) Patch() (*User, error) {
-	return u, ErrorNotImplemented
+	db := u.DB()
+
+	if u.NilUUID() {
+		return u, ErrorUserNotSpecified
+	}
+
+	fields := make([]interface{}, 0)
+	query := "update users set"
+	qNum := 0
+
+	addQuery := func(s string, i interface{}) {
+		qNum++
+		query += " " + s + "=$" + strconv.Itoa(qNum) + ","
+		fields = append(fields, i)
+	}
+
+	if u.Created.Valid() {
+		addQuery("created", u.Created)
+	}
+
+	if u.DisplayName != "" { // TODO: Validate display names
+		addQuery("display_name", u.DisplayName)
+	}
+
+	if u.Birth.Valid() { // TODO: Validate DOB
+		u.Age.Update(u.Birth)
+		addQuery("date_of_birth", u.Birth)
+		addQuery("age", u.Age)
+	}
+
+	if u.Age.Valid() && !u.Birth.Valid() {
+		return u, ErrorUserBirthNotSpecified
+	}
+
+	if u.Height.Valid() {
+		addQuery("height", u.Height)
+	}
+
+	if u.Weight.Valid() {
+		addQuery("height", u.Height)
+	}
+
+	// TODO: Impl medication
+	// TODO: Impl preferences
+
+	query = strings.TrimSuffix(query, ",")
+	qNum++
+	query += " where id=$;" + strconv.Itoa(qNum)
+	fields = append(fields, u.ID)
+
+	_, err := db.Exec(context.Background(), query, fields...)
+
+	if err != nil {
+		u.Logger.Warnw("Failed to update user in DB", zap.Error(err))
+		_ = db.Rollback(context.Background())
+		return u, err
+	}
+
+	// Want to commit before Get()
+	db.Commit(context.Background())
+	return u.Get()
 }
 
 func (u *User) Delete() (*User, error) {
