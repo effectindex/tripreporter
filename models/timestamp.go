863cfbab4e261c20b1f1fea7fe692b6450f1a032
5HT2 <git@frogg.ie>
2022-11-19 12:41:45 -0800

    models: Rewrite `Timestamp` (formerly `Time`), fix scanning from DB, add parsing, default and validation methods

---
+++ models/timestamp.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"fmt"
+	"github.com/jackc/pgtype"
+	"time"
+)
+
+type Timestamp struct {
+	pgtype.Timestamptz
+}
+
+// Valid returns if the time is valid
+func (t *Timestamp) Valid() bool {
+	if t == nil {
+		return false
+	}
+
+	return !t.Time.IsZero()
+}
+
+// Default will set Timestamp to it's default value
+func (t *Timestamp) Default() {
+	*t = Timestamp{}
+}
+
+// Now set the value to the current time
+func (t *Timestamp) Now() {
+	*t = Timestamp{pgtype.Timestamptz{Time: time.Now()}}
+}
+
+// Parse creates a Timestamp from a string
+func (t *Timestamp) Parse(s string) (*Timestamp, error) {
+	if t1, err := time.ParseInLocation(time.RFC3339, s, time.UTC); err != nil {
+		return nil, err
+	} else {
+		*t = Timestamp{pgtype.Timestamptz{Time: t1}}
+		return t, nil
+	}
+}
+
+// ParseDate creates a Timestamp from a string and a 2006-01-02 formatted date.
+func (t *Timestamp) ParseDate(s string) (*Timestamp, error) {
+	return t.Parse(s + "T00:00:00Z")
+}
+
+// String formats models.Timestamp in a format usable by the Postgres Timestamptz type
+func (t *Timestamp) String() string {
+	return t.Time.Format(time.RFC3339)
+}
+
+// Scan implements the database/sql Scanner interface. This copies pgtype.Timestamptz and modifies it to scan as time.UTC
+func (t *Timestamp) Scan(src interface{}) error {
+	if src == nil {
+		*t = Timestamp{pgtype.Timestamptz{Status: pgtype.Null}}
+		return nil
+	}
+
+	var err error
+	switch src := src.(type) {
+	case string:
+		err = t.DecodeText(nil, []byte(src))
+	case []byte:
+		srcCopy := make([]byte, len(src))
+		copy(srcCopy, src)
+		err = t.DecodeText(nil, srcCopy)
+	case time.Time:
+		// Scan into UTC and not current timezone
+		*t = Timestamp{pgtype.Timestamptz{Time: src.In(time.UTC), Status: pgtype.Present}}
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// Scan into UTC and not current timezone
+	if t.Valid() {
+		*t = Timestamp{pgtype.Timestamptz{Time: t.Time.In(time.UTC), Status: t.Status, InfinityModifier: t.InfinityModifier}}
+	}
+
+	return fmt.Errorf("cannot scan %T", src)
+}
