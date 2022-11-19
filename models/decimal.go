556df37141ba8d233e9603e44eb8986b74b20962
5HT2 <git@frogg.ie>
2022-11-19 12:50:23 -0800

    models: Add `Decimal`

---
+++ models/decimal.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"github.com/shopspring/decimal"
+	"strconv"
+)
+
+type Decimal struct {
+	decimal.Decimal
+}
+
+func (d *Decimal) Valid() bool {
+	return d != nil && !d.IsZero()
+}
+
+func (d *Decimal) Set(whole, point int) (*Decimal, error) {
+	if d1, err := decimal.NewFromString(strconv.Itoa(whole) + "." + strconv.Itoa(point)); err != nil {
+		return nil, err
+	} else {
+		*d = Decimal{d1}
+		return d, nil
+	}
+}
+
+func (d *Decimal) Get() *Decimal {
+	return d
+}
+
+func (d *Decimal) Default() {
+	func(x **Decimal) {
+		*x = nil
+	}(&d)
+}
