5f2c5c6a11206e9a20dfd261a6c13ee6f644813b
5HT2 <git@frogg.ie>
2022-11-27 22:59:49 -0800

    models: (timestamp) Fix marshalling when Timestamp is entirely nil or Timestamp.Status is pgtype.Undefined

--- models/timestamp.go
+++ models/timestamp.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -17,17 +18,17 @@ func (t *Timestamp) Valid() bool {
 		return false
 	}
 
-	return !t.Time.IsZero()
+	return !t.Time.IsZero() && t.Status != pgtype.Undefined
 }
 
 // Default will set Timestamp to it's default value
 func (t *Timestamp) Default() {
-	*t = Timestamp{}
+	*t = Timestamp{pgtype.Timestamptz{Status: pgtype.Null}}
 }
 
 // Now set the value to the current time
 func (t *Timestamp) Now() {
-	*t = Timestamp{pgtype.Timestamptz{Time: time.Now()}}
+	*t = Timestamp{pgtype.Timestamptz{Time: time.Now(), Status: pgtype.Present}}
 }
 
 // Parse creates a Timestamp from a string
@@ -35,7 +36,7 @@ func (t *Timestamp) Parse(s string) (*Timestamp, error) {
 	if t1, err := time.ParseInLocation(time.RFC3339, s, time.UTC); err != nil {
 		return nil, err
 	} else {
-		*t = Timestamp{pgtype.Timestamptz{Time: t1}}
+		*t = Timestamp{pgtype.Timestamptz{Time: t1, Status: pgtype.Present}}
 		return t, nil
 	}
 }
@@ -81,4 +82,29 @@ func (t *Timestamp) Scan(src interface{}) error {
 	}
 
 	return fmt.Errorf("cannot scan %T", src)
+}
+
+// MarshalJSON implements the encoding/json Marshaler interface. This copies pgtype.Timestamptz and modifies it to treat pgtype.Undefined as pgtype.Null
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	switch t.Status {
+	case pgtype.Null, pgtype.Undefined:
+		return []byte("null"), nil
+	}
+
+	if t.Status != pgtype.Present {
+		return nil, ErrorGenericUnknown
+	}
+
+	var s string
+
+	switch t.InfinityModifier {
+	case pgtype.None:
+		s = t.Time.Format(time.RFC3339Nano)
+	case pgtype.Infinity:
+		s = "infinity"
+	case pgtype.NegativeInfinity:
+		s = "-infinity"
+	}
+
+	return json.Marshal(s)
 }
