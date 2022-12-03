e2ad7dc2e5c93d86b41782f55088d943184ef089
5HT2 <git@frogg.ie>
2022-12-03 06:35:48 -0800

    impr: (builtin) Add `SliceEqual()`

---
+++ util/builtin.go
@@ -0,0 +1,14 @@
+package util
+
+// SliceEqual returns true if all bytes of a and b are the same
+func SliceEqual[T comparable](a []T, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
