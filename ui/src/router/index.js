c40f717311f7f506d0313bcb954654df4d828d5f
5HT2 <git@frogg.ie>
2022-12-16 21:26:22 -0800

    ui: (router) Add `/login`

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
         component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     },
     {
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    },
+    {
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: () => import('@/views/NotFound.vue')
