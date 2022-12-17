cd4af31301f026b22f12fa59172fe6bdbfa2974e
5HT2 <git@frogg.ie>
2022-12-16 20:39:34 -0800

    ui: (App) Add login page + nicer colors

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link>
   </nav>
   <router-view/>
 </template>
@@ -21,10 +22,10 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #2D2D34;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #BF075A;
 }
 </style>
