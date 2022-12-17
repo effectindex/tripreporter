9d7eb3e6596212825ab3bd68a3d2d043597aa8da
5HT2 <git@frogg.ie>
2022-12-16 20:52:43 -0800

    ui: (LoginView) Boilerplate export

---
+++ ui/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <h1>This is a login page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
+
+<style scoped>
+
+</style>
