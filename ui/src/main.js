bbd43b701d071ff28507340a262c680c6ec66433
5HT2 <git@frogg.ie>
2023-01-07 15:53:56 -0800

    fix: FormKit theme not working

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@formkit/themes/genesis'
+import { defaultConfig, plugin } from '@formkit/vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(plugin, defaultConfig({theme: 'genesis'}))
+    .mount('#app')
