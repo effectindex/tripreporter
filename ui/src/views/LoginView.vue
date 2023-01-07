dd4f9a05ac777a61bcc79d999532cefa49211334
5HT2 <git@frogg.ie>
2023-01-07 15:54:36 -0800

    ui: Add login form

--- ui/src/views/LoginView.vue
+++ ui/src/views/LoginView.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="login">
-    <h1>This is a login page</h1>
+    <h1>Login to your TripReporter Account</h1>
+
+    <div class="LoginView__form">
+      <FormKit
+          type="text"
+          name="username"
+          id="username"
+          label="Username"
+          validation="required"
+          placeholder="mark76"
+      />
+
+      <FormKit
+          type="password"
+          name="password"
+          id="password"
+          label="Password"
+          validation="required"
+          placeholder="trmark76&!"
+      />
+    </div>
   </div>
 </template>
 
@@ -11,5 +31,8 @@ export default {
 </script>
 
 <style scoped>
-
+.LoginView__form {
+  max-width: 25em;
+  margin: auto;
+}
 </style>
