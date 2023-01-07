a9364a233d9ebbe81d3a41a373a089ad8154a606
5HT2 <git@frogg.ie>
2023-01-07 15:54:43 -0800

    ui: Add signup form

---
+++ ui/src/views/SignupView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="signup">
+    <h1>Create your TripReporter Account</h1>
+
+    <div class="SignupView__form">
+      <!-- TODO: Make optional.
+      -->
+      <FormKit
+          type="email"
+          name="email"
+          id="email"
+          label="Email address"
+          help="Used for password recovery. Optional."
+          validation="required|email"
+          placeholder="example@effectindex.com"
+      />
+
+      <!-- TODO: How do we handle errors here?
+           TODO: The validation obviously isn't perfect, as it's quite annoying to replicate the ruleset with regex.
+           TODO: We can use API calls as validation with FormKit, so we should use our backend in the future instead.
+      -->
+      <FormKit
+          type="text"
+          name="username"
+          id="username"
+          label="Username"
+          help="Used to login. You can use letters, numbers and symbols."
+          validation="required|length:3,32|matches:/^[a-z0-9_-]+$/"
+          :validation-messages="{ matches: 'Must contain only lowercase letters, numbers, underscores or dashes.' }"
+          placeholder="mark76"
+      />
+
+      <FormKit
+          type="password"
+          name="password"
+          id="password"
+          label="Password"
+          help="Used to login. Must contain at least 2 symbols and 3 letters / numbers."
+          validation="required|length:3,32|matches:/^[a-z0-9_-]+$/"
+          placeholder="trmark76&!"
+      />
+
+      <FormKit
+          type="text"
+          name="name"
+          id="name"
+          label="Display name"
+          help="Shown to other users when viewing your profile."
+          validation="required"
+          placeholder="“Mark Gillis”"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignupView"
+}
+</script>
+
+<style scoped>
+.SignupView__form {
+  max-width: 25em;
+  margin: auto;
+}
+</style>
