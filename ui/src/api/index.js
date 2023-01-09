62b3f071eddbd936961e81ac12cb101902e7cbca
5HT2 <git@frogg.ie>
2023-01-08 21:43:10 -0800

    ui: (api) Set `Content-Type`

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:3000/api/v1',
+    headers: {
+        "Content-Type": "application/json",
+    },
 })
 
 export default apiClient
