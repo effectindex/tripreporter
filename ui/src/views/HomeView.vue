b8c055061f59fc793f01d70b192752841a165707
5HT2 <git@frogg.ie>
2023-01-14 16:57:06 -0800

    ui: (HomeView) Remove placeholder Lorem ipsum

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -2,19 +2,7 @@
   <div class="Home">
     <h1>Home</h1>
     <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet.
-    </p>
-
-    <h2>Amet sit</h2>
-    <p>
-      Eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet.
+      Welcome to the TripReporter home page.
     </p>
   </div>
 </template>
