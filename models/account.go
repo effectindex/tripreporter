575eae8d88593ac9633d17511aeed16bdb2ff62b
5HT2 <git@frogg.ie>
2023-01-14 16:53:56 -0800

    models: (account) Fix default `AccountConfig` Symbol vs NonSymbol being flipped around

--- models/account.go
+++ models/account.go
@@ -50,11 +50,11 @@ func SetupAccountConfig(ctx types.Context) {
 			Message:            "a-z 0-9 _ -",
 			Allowed: allowedChars{
 				Symbol: map[string]bool{
-					"a": true, "b": true, "c": true, "d": true, "e": true, "f": true, "g": true, "h": true, "i": true, "j": true, "k": true, "l": true, "m": true, "n": true, "o": true, "p": true, "q": true, "r": true, "s": true, "t": true, "u": true, "v": true, "w": true, "x": true, "y": true, "z": true,
-					"0": true, "1": true, "2": true, "3": true, "4": true, "5": true, "6": true, "7": true, "8": true, "9": true,
+					"_": true, "-": true,
 				},
 				NonSymbol: map[string]bool{
-					"_": true, "-": true,
+					"a": true, "b": true, "c": true, "d": true, "e": true, "f": true, "g": true, "h": true, "i": true, "j": true, "k": true, "l": true, "m": true, "n": true, "o": true, "p": true, "q": true, "r": true, "s": true, "t": true, "u": true, "v": true, "w": true, "x": true, "y": true, "z": true,
+					"0": true, "1": true, "2": true, "3": true, "4": true, "5": true, "6": true, "7": true, "8": true, "9": true,
 				},
 			},
 		},
@@ -67,12 +67,12 @@ func SetupAccountConfig(ctx types.Context) {
 			Message:            "A-Z a-z 0-9 _ - ! @ # $ % ^ & * ( ) + = space",
 			Allowed: allowedChars{
 				Symbol: map[string]bool{
+					"_": true, "-": true, "!": true, "@": true, "#": true, "$": true, "%": true, "^": true, "&": true, "*": true, "(": true, ")": true, "+": true, "=": true, " ": true,
+				},
+				NonSymbol: map[string]bool{
 					"A": true, "B": true, "C": true, "D": true, "E": true, "F": true, "G": true, "H": true, "I": true, "J": true, "K": true, "L": true, "M": true, "N": true, "O": true, "P": true, "Q": true, "R": true, "S": true, "T": true, "U": true, "V": true, "W": true, "X": true, "Y": true, "Z": true,
 					"a": true, "b": true, "c": true, "d": true, "e": true, "f": true, "g": true, "h": true, "i": true, "j": true, "k": true, "l": true, "m": true, "n": true, "o": true, "p": true, "q": true, "r": true, "s": true, "t": true, "u": true, "v": true, "w": true, "x": true, "y": true, "z": true,
 					"0": true, "1": true, "2": true, "3": true, "4": true, "5": true, "6": true, "7": true, "8": true, "9": true,
-				},
-				NonSymbol: map[string]bool{
-					"_": true, "-": true, "!": true, "@": true, "#": true, "$": true, "%": true, "^": true, "&": true, "*": true, "(": true, ")": true, "+": true, "=": true, " ": true,
 				},
 			},
 		},
