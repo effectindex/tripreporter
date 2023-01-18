effectindex/tripreporter

commit be244ce3404c3bb88277297d617559f8b6fb53dd

api: Nicer endpoint registration setup

api/api.go
@@ -27,33 +27,36 @@ func Setup(isDevelopment bool, logger *zap.SugaredLogger) {
 
 // Handler manages all paths, with Router handling anything not defined here.
 func Handler() http.Handler {
-	mux := mux.NewRouter()
+	router := mux.NewRouter()
 
 	// let api.Router do everything else
-	mux.HandleFunc("/", Router)
+	router.HandleFunc("/", Router)
 
 	// serve /static/ by cache in production (no hot-reload support)
 	if !dev { // if running in development mode, let api.Router reverse proxy it
 		staticFS, _ := fs.Sub(ui.StaticFiles, "dist")
 		httpFS := http.FileServer(http.FS(staticFS))
-		mux.Handle("/static/", httpFS)
+		router.Handle("/static/", httpFS)
 	}
 
 	// Redirect /api with no trailing slash to the documentation url
-	mux.Handle("/api", http.RedirectHandler(os.Getenv("DOCS_URL"), http.StatusMovedPermanently))
+	router.Handle("/api", http.RedirectHandler(os.Getenv("DOCS_URL"), http.StatusMovedPermanently))
 
 	// API functions
-	vX := mux.PathPrefix("/api/").Subrouter()
+	vX := router.PathPrefix("/api/").Subrouter()
 	vX.MethodNotAllowedHandler = ctx.HandleFunc(MsgMethodNotAllowed)
 	vX.NotFoundHandler = ctx.HandleFunc(MsgInvalidApiVersion)
 
+	// API v1 methods
 	v1 := vX.PathPrefix("/v1").Subrouter()
-	v1.HandleFunc("/session", SessionPost).Methods(http.MethodPost)
-	v1.HandleFunc("/session/{id}", SessionGet).Methods(http.MethodGet)
 	v1.MethodNotAllowedHandler = ctx.HandleFunc(MsgMethodNotAllowed)
 	v1.NotFoundHandler = ctx.HandleFunc(MsgInvalidEndpoint)
 
-	return mux
+	// API v1 endpoints
+	SetupAccountEndpoints(v1)
+	SetupSessionEndpoints(v1)
+
+	return router
 }
 
 // Router will route everything except /static/ and valid /api/ endpoints.

api/session.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func SetupSessionEndpoints(v1 *mux.Router) {
+	v1.HandleFunc("/session", SessionPost).Methods(http.MethodPost)
+	v1.HandleFunc("/session/{id}", SessionGet).Methods(http.MethodGet)
+}
+
 // SessionPost path is /api/v1/session
 func SessionPost(w http.ResponseWriter, r *http.Request) {
 	_ = mux.Vars(r)

commit 13f4aeaf43557eb2dfb7f463f46c839a200dd4db

api: Fix production not serving static files properly.
Closes #46.
This was caused by be244ce3404c3bb88277297d617559f8b6fb53dd, where `router.Handle("/static/", httpFS)` should have been changed to `router.PathPrefix("/static/").HandlerFunc(httpFS)`.

api/api.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"strings"
 
 	"github.com/effectindex/tripreporter/ui"
 	"github.com/effectindex/tripreporter/util"
@@ -25,7 +24,8 @@ func Setup(isDevelopment bool, logger *zap.SugaredLogger) {
 	dev = isDevelopment
 }
 
-// Handler manages all paths, with Router handling anything not defined here.
+// Handler will serve /api, and pass the rest off to Router.
+// In production, Handler will also serve /static/.
 func Handler() http.Handler {
 	router := mux.NewRouter()
 
@@ -33,7 +33,9 @@ func Handler() http.Handler {
 	if !dev { // if running in development mode, let api.Router reverse proxy it
 		staticFS, _ := fs.Sub(ui.StaticFiles, "dist")
 		httpFS := http.FileServer(http.FS(staticFS))
-		router.Handle("/static/", httpFS)
+		router.PathPrefix("/static/").HandlerFunc(ctx.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx.Logger.Debugw("Serving static", "path", r.URL.Path)
+		}, httpFS))
 	}
 
 	// Redirect /api with no trailing slash to the documentation url
@@ -41,36 +43,31 @@ func Handler() http.Handler {
 
 	// API functions
 	vX := router.PathPrefix("/api/").Subrouter()
-	vX.MethodNotAllowedHandler = ctx.HandleFunc(MsgMethodNotAllowed)
-	vX.NotFoundHandler = ctx.HandleFunc(MsgInvalidApiVersion)
+	vX.MethodNotAllowedHandler = ctx.HandleMessage(MsgMethodNotAllowed)
+	vX.NotFoundHandler = ctx.HandleMessage(MsgInvalidApiVersion)
 
 	// API v1 methods
 	v1 := vX.PathPrefix("/v1").Subrouter()
-	v1.MethodNotAllowedHandler = ctx.HandleFunc(MsgMethodNotAllowed)
-	v1.NotFoundHandler = ctx.HandleFunc(MsgInvalidEndpoint)
+	v1.MethodNotAllowedHandler = ctx.HandleMessage(MsgMethodNotAllowed)
+	v1.NotFoundHandler = ctx.HandleMessage(MsgInvalidEndpoint)
 
 	// API v1 endpoints
 	SetupAccountEndpoints(v1)
 	SetupSessionEndpoints(v1)
 	SetupUserEndpoints(v1)
 
-	// let api.Router do everything else
+	// Let api.Router do everything else, including serving /static/ in development
 	router.PathPrefix("/").HandlerFunc(Router)
 
 	return router
 }
 
-// Router will route everything except /static/ and valid /api/ endpoints.
+// Router will route /, /favicon.ico and anything not handled by Handler.
+// In development, Router will also handle /static/.
 func Router(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintln(w, http.StatusText(http.StatusMethodNotAllowed))
-		return
-	}
-
-	// For API endpoints not already handled in Handler()
-	if strings.HasPrefix(r.URL.Path, "/api") {
-		http.NotFound(w, r)
 		return
 	}
 