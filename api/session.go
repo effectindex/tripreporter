c723a9eef1ebe9977d4c255e93ebd8ca31b9237b
5HT2 <git@frogg.ie>
2023-03-06 18:07:59 -0500

    api: (session) Implement `/session/validate`

--- api/session.go
+++ api/session.go
@@ -7,8 +7,12 @@ import (
 )
 
 func SetupSessionEndpoints(v1 *mux.Router) {
+	a1 := v1.Methods(http.MethodGet, http.MethodPatch, http.MethodDelete).Subrouter()
+	a1.Use(AuthMiddleware())
+
 	v1.HandleFunc("/session", SessionPost).Methods(http.MethodPost)
 	v1.HandleFunc("/session/{id}", SessionGet).Methods(http.MethodGet)
+	a1.HandleFunc("/session/validate", SessionGetValidate).Methods(http.MethodGet)
 }
 
 // SessionPost path is /api/v1/session
@@ -29,4 +33,11 @@ func SessionGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx.Logger.Infow("Got ID", "id", id)
+}
+
+// SessionGetValidate path is /api/v1/session/validate
+func SessionGetValidate(w http.ResponseWriter, r *http.Request) {
+	// This might look like it does nothing, but it is called with the AuthMiddleware, which means if you don't have
+	// a valid session it will return a 403 from there, instead.
+	ctx.Handle(w, r, MsgOk)
 }
