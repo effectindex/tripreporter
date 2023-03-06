1b3eba7379acb6a31e5422a65acd6c178c66ade6
5HT2 <git@frogg.ie>
2023-03-06 18:23:25 -0500

    ui: (lib) Add event bus

---
+++ ui/src/assets/lib/event.js
@@ -0,0 +1,13 @@
+import { ref } from "vue";
+const bus = ref(new Map());
+
+export default function useEventsBus(){
+    function emit(event, ...args) {
+        bus.value.set(event, args);
+    }
+
+    return {
+        emit,
+        bus
+    }
+}
