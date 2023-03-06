c74287eda24143e0168efb9084208b8502ed0dd3
5HT2 <git@frogg.ie>
2023-03-06 18:26:50 -0500

    ui: (lib) Cleanup debug logging

--- ui/src/assets/lib/logger.js
+++ ui/src/assets/lib/logger.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-console */
 
 export default function log(message, ...optionalParams) {
-    console.log(`BASE_URL: ${process.env.BASE_URL}`)
-    console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
-
     if (process.env.NODE_ENV !== "production") {
         console.log(message, ...optionalParams)
     }
