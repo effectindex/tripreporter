4d1ebcb9e3428015fe8ab71a6ec0b1243628f375
5HT2 <git@frogg.ie>
2023-03-06 18:06:19 -0500

    impr: Provide pinia globally

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { defaultConfig, plugin } from '@formkit/vue'
 import App from './App.vue'
 import router from './router'
 import apiClient from '@/api'
 
+const pinia = createPinia()
 const app = createApp(App)
+    .use(pinia)
     .use(router)
     .use(plugin, defaultConfig({theme: 'genesis'}))
     .provide('axios', apiClient)
