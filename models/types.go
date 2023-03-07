312c03bbce131962a2d689989b09c6e237f97419
5HT2 <git@frogg.ie>
2023-03-07 12:37:09 -0500

    types: Cleanup unused types

--- models/types.go
+++ models/types.go
@@ -1,9 +1,5 @@
 package models
 
-import (
-	"time"
-)
-
 type RouteOfAdministration int64
 
 const ( // TODO: check if complete
@@ -39,12 +35,9 @@ const (
 	UnitImperial
 )
 
-type UserMedication struct { // TODO: How do we feel about this being unencrypted?
-	Medications []Medication `json:"medications,omitempty"`
-}
-
-type UserPreferences struct { // TODO: How do we feel about this being unencrypted?
-	Timezone     time.Location `json:"timezone,omitempty"`      // Default: Europe/London
-	HeightFormat DisplayUnit   `json:"height_format,omitempty"` // Default: UnitMetric // Display height in centimeters or feet + inches
-	WeightFormat DisplayUnit   `json:"weight_format,omitempty"` // Default: UnitMetric // Display weight in kilograms or pounds
-}
+// TODO: Impl
+//type UserPreferences struct { // TODO: How do we feel about this being unencrypted?
+//	Timezone     time.Location `json:"timezone,omitempty"`      // Default: Europe/London
+//	HeightFormat DisplayUnit   `json:"height_format,omitempty"` // Default: UnitMetric // Display height in centimeters or feet + inches
+//	WeightFormat DisplayUnit   `json:"weight_format,omitempty"` // Default: UnitMetric // Display weight in kilograms or pounds
+//}
