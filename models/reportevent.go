b5d51898a8c9957303aeb3d2e926b6ff4aadd259
5HT2 <git@frogg.ie>
2023-03-07 12:37:50 -0500

    models: (reportevent) Update to match DB

--- models/reportevent.go
+++ models/reportevent.go
@@ -1,19 +1,23 @@
 package models
 
+import "sort"
+
 type ReportEvent struct {
-	Unique
-	ReportEventType    `json:"type" db:"type"`
-	Timestamp          `json:"timestamp" db:"timestamp"`
-	ReportEventSection `json:"section" db:"section"`
-	Content            string `json:"content" db:"content"`
+	Unique    `json:"report_id" db:"report_id"` // References the original report ID
+	Index     int64                             `json:"index" db:"event_index"`         // Order of sections
+	Timestamp Timestamp                         `json:"timestamp" db:"event_timestamp"` // Time of event
+	Type      ReportEventType                   `json:"type" db:"event_type"`           // Type of event
+	Section   ReportEventSection                `json:"section" db:"event_section"`     // Section event is in
+	Content   string                            `json:"content" db:"event_content"`     // Content of event, if ReportEventNote
+	Drug      Drug                              `json:"drug,omitempty" db:"event_drug"` // Drug of event, if ReportEventDrug
 }
 
 type ReportEventType int64
 
 const (
-	ReportEventUnknown   ReportEventType = iota
-	ReportEventNote                      = 1
-	ReportEventSubstance                 = 2
+	ReportEventUnknown ReportEventType = iota
+	ReportEventNote                    = 1
+	ReportEventDrug                    = 2
 )
 
 type ReportEventSection int64
@@ -25,3 +29,11 @@ const (
 	ReportEventSectionPeak
 	ReportEventSectionOffset
 )
+
+type ReportEvents []*ReportEvent
+
+func (r ReportEvents) Sort() {
+	sort.Slice(r, func(i, j int) bool {
+		return r[i].Index < r[j].Index
+	})
+}
