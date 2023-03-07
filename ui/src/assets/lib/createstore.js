60e5ef1b7920eb1bff527181340ba45249bee0f5
5HT2 <git@frogg.ie>
2023-03-07 16:03:42 -0500

    ui: (lib) Implement create store

---
+++ ui/src/assets/lib/createstore.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+
+export const useCreateStore = defineStore('create', {
+    state: () => {
+        return {
+            submitClass: 'formkit-outer',
+            page: 0
+        }
+    }
+})
