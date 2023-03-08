bf84795b47bfe94b60c3a96d2bcf851b8c113407
5HT2 <git@frogg.ie>
2023-03-07 19:23:58 -0500

    models: (drug) Update to use new DrugPrescribed type, remove `account_id` from JSON

--- models/drug.go
+++ models/drug.go
@@ -4,11 +4,11 @@ import "time"
 
 type Drug struct {
 	Unique
-	Account    Unique                `json:"account_id" db:"account_id"`              // References account that created this drug.
+	Account    Unique                `db:"account_id"`                                // References account that created this drug.
 	Name       string                `json:"name" db:"drug_name"`                     // Required
 	Dosage     int64                 `json:"dosage,omitempty" db:"drug_dosage"`       // Optional
 	DosageUnit DosageUnit            `json:"dosage_unit" db:"drug_dosage_unit"`       // Required, use DosageUnknown if unset
 	RoA        RouteOfAdministration `json:"roa" db:"drug_roa"`                       // Required, use RoaOther if unset
 	Frequency  time.Duration         `json:"frequency,omitempty" db:"drug_frequency"` // Optional, uses 0 if unset
-	Prescribed bool                  `json:"prescribed" db:"drug_prescribed"`         // Optional, default false
+	Prescribed DrugPrescribed        `json:"prescribed" db:"drug_prescribed"`         // Optional, default false
 }
