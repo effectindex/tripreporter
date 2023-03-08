bedb5ed0464852034f85da7ec2130c0aa31fa44b
5HT2 <git@frogg.ie>
2023-03-07 20:30:31 -0500

    ui: (views) Update strings for login and signup

--- ui/src/views/SignupView.vue
+++ ui/src/views/SignupView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="signup">
-    <h1>Create your <span class="--tr-muted-text">subjective.report</span> Account</h1>
+    <h1>Create a <span class="--tr-muted-text">subjective.report</span> account 🚀</h1>
 
     <div class="DefaultView__message" id="DefaultView__message">
       <div class="DefaultView__message_text" id="DefaultView__message_text"></div>
