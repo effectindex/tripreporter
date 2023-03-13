2750932372a9ff489c89eee192187883b0e072d9
5HT2 <git@frogg.ie>
2023-03-13 18:11:42 -0400

    impr: Provide Formkit Pro globally

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,15 +1,19 @@
+import '@formkit/themes/genesis'
+import '@formkit/pro/genesis'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { defaultConfig, plugin } from '@formkit/vue'
+import { createProPlugin, inputs } from '@formkit/pro'
 import App from './App.vue'
 import router from './router'
 import apiClient from '@/api'
 
+const formkitPro = createProPlugin(process.env.FORMKIT_API_KEY, inputs)
 const pinia = createPinia()
 const app = createApp(App)
     .use(pinia)
     .use(router)
-    .use(plugin, defaultConfig({theme: 'genesis'}))
+    .use(plugin, defaultConfig({ plugins: [formkitPro()] }))
     .provide('axios', apiClient)
 
 app.mount('#app')
