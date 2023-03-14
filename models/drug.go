9a0c1e3264a93a7e010306a740b32019158c3b3b
5HT2 <git@frogg.ie>
2023-03-14 19:13:20 -0400

    models: (drug) Switch `DosageUnit` to a string, add `account_id` to JSON output, add `ParseDose()`

--- models/drug.go
+++ models/drug.go
@@ -1,14 +1,44 @@
 package models
 
-import "time"
+import (
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/effectindex/tripreporter/types"
+)
+
+var (
+	doseRegex = regexp.MustCompile(`([0-9]+)?(\s*)(.*)`)
+)
 
 type Drug struct {
 	Unique
-	Account    Unique                `db:"account_id"`                                // References account that created this drug.
-	Name       string                `json:"name" db:"drug_name"`                     // Required
-	Dosage     int64                 `json:"dosage,omitempty" db:"drug_dosage"`       // Optional
-	DosageUnit DosageUnit            `json:"dosage_unit" db:"drug_dosage_unit"`       // Required, use DosageUnknown if unset
-	RoA        RouteOfAdministration `json:"roa" db:"drug_roa"`                       // Required, use RoaOther if unset
-	Frequency  time.Duration         `json:"frequency,omitempty" db:"drug_frequency"` // Optional, uses 0 if unset
-	Prescribed DrugPrescribed        `json:"prescribed" db:"drug_prescribed"`         // Optional, default false
+	Account    Unique                `json:"account_id" db:"account_id"`        // References account that created this drug.
+	Name       string                `json:"name" db:"drug_name"`               // Required
+	Dosage     int64                 `json:"dosage" db:"drug_dosage"`           // Optional
+	DosageUnit string                `json:"dosage_unit" db:"drug_dosage_unit"` // Optional, uses "unknown" if unset
+	RoA        RouteOfAdministration `json:"roa" db:"drug_roa"`                 // Required, uses RoaUnknown if unset
+	Frequency  time.Duration         `json:"frequency" db:"drug_frequency"`     // Optional, uses 0 if unset
+	Prescribed DrugPrescribed        `json:"prescribed" db:"drug_prescribed"`   // Optional, uses DrugPrescribedUnknown if unset
+}
+
+func (d *Drug) ParseDose(dose string) error {
+	parts := doseRegex.FindStringSubmatch(dose)
+	if len(parts) == 1 {
+		return types.ErrorDrugParseLen
+	}
+
+	dosage := 0
+	if len(parts) > 3 {
+		d, err := strconv.Atoi(parts[1])
+		if err == nil {
+			dosage = d
+		}
+	}
+
+	d.Dosage = int64(dosage)
+	d.DosageUnit = parts[3]
+
+	return nil
 }
