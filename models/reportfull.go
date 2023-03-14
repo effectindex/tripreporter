2e7e03cf37c8b88506af2df0e374dbaca537a10a
5HT2 <git@frogg.ie>
2023-03-14 19:17:14 -0400

    models: (reportfull) Fully implement `FromBody()`

--- models/reportfull.go
+++ models/reportfull.go
@@ -84,22 +84,39 @@ func (r *ReportFull) FromBody(r1 *http.Request) (*ReportFull, error) {
 	r.InitType(r)
 
 	type ReportFormEvent struct {
-		Timestamp  string `json:"timestamp"`
+		Timestamp  string `json:"timestamp,omitempty"`
 		IsDrug     bool   `json:"is_drug,omitempty"`
-		Section    int64  `json:"section,omitempty"`
+		Section    int64  `json:"section,string,omitempty"`
 		Content    string `json:"content,omitempty"`
 		DrugName   string `json:"drug_name,omitempty"`
 		DrugDosage string `json:"drug_dosage,omitempty"`
-		RoA        int64  `json:"roa,omitempty"`
-		Prescribed int64  `json:"prescribed,omitempty"`
+		RoA        int64  `json:"roa,string,omitempty"`
+		Prescribed int64  `json:"prescribed,string,omitempty"`
 	}
 
 	type ReportForm struct {
-		ReportSections []ReportFormEvent `json:"report_sections"`
 		Title          string            `json:"title"`
 		Setting        string            `json:"setting,omitempty"`
 		ReportDate     string            `json:"report_date"`
+		ReportSections []ReportFormEvent `json:"report_sections,omitempty"`
 	}
+
+	// We need a report ID in order to parse the report sections.
+	// Anything calling this method should init an ID manually, unless making a new report.
+	if r.NilUUID() {
+		err := r.InitUUID(r.Logger)
+		if err != nil {
+			return r, err
+		}
+	}
+
+	rCtx := r1.Context()
+	ctxValues, ok := rCtx.Value(ContextValuesKey).(*ContextValues)
+	if !ok {
+		return r, types.ErrorContextCastFailed
+	}
+
+	accountID := ctxValues.SessionClaims.Account.UUID
 
 	if r1.Body == nil {
 		return r, types.ErrorStringEmpty.PrefixedError("Request body")
@@ -121,9 +138,104 @@ func (r *ReportFull) FromBody(r1 *http.Request) (*ReportFull, error) {
 		return r, err
 	}
 
-	r.Logger.Debugw("ReportFull.FromBody", "ReportForm", rf)
-	// TODO
-	//r.FromData(a1)
+	//
+	// Now we should have all the data, we need to turn some types into Go types to make sense.
+
+	// First lets fix the create and last modified timestamps, if they're not valid
+	if !r.Created.Valid() {
+		r.Created.Now()
+	}
+
+	if !r.LastModified.Valid() {
+		r.LastModified = r.Created
+	}
+
+	// Now we want to trim empty sections from the array
+	formSections := rf.ReportSections[:0]
+	for _, s := range rf.ReportSections {
+		sectionEmpty := false
+
+		if s.IsDrug {
+			if s.Section == 0 && len(s.DrugName) == 0 && len(s.DrugDosage) == 0 && s.RoA == 0 && s.Prescribed == 0 {
+				sectionEmpty = true
+			}
+		} else {
+			if s.Section == 0 && len(s.Content) == 0 {
+				sectionEmpty = true
+			}
+		}
+
+		// Keep non-empty sections
+		if !sectionEmpty {
+			formSections = append(formSections, s)
+		}
+	}
+
+	// Only keep non-empty sections
+	rf.ReportSections = formSections
+
+	// Try to find if any of the timestamps have been set
+	firstTimestamp := "T00:00:00Z"
+	for _, s := range rf.ReportSections {
+		if len(s.Timestamp) > 0 {
+			firstTimestamp = "T" + s.Timestamp + ":00Z"
+			break
+		}
+	}
+
+	// Now lets parse the rf.ReportDate as an actual Timestamp
+	date, err := r.Date.Parse(rf.ReportDate + firstTimestamp)
+	if err != nil {
+		return r, err
+	}
+
+	r.Date = *date
+
+	// Now we can parse the sections properly
+	sections := make(ReportEvents, 0)
+	accountUnique := Unique{ID: accountID}
+	accountUnique.InitType(&Account{})
+
+	for n, s := range rf.ReportSections {
+		event := &ReportEvent{
+			Unique:  r.Unique,
+			Index:   int64(n),
+			Type:    ReportEventNote,
+			Content: s.Content,
+		}
+
+		if s.IsDrug {
+			event.Type = ReportEventDrug
+			event.Drug = Drug{ // TODO: Frequency is not parsed here
+				Account:    accountUnique,
+				Name:       s.DrugName,
+				RoA:        RouteOfAdministration(s.RoA),
+				Prescribed: DrugPrescribed(s.Prescribed),
+			}
+			event.Drug.ParseDose(s.DrugDosage)
+			event.Drug.InitType(event.Drug)
+
+			if err != nil {
+				return r, err
+			}
+
+			err = event.Drug.InitUUID(r.Logger)
+			if err != nil {
+				return r, err
+			}
+		}
+
+		event.Section = ReportEventSection(s.Section)
+
+		sections = append(sections, event)
+	}
+
+	// Set title, sections and events now that we have the sections parsed
+	r.Title = rf.Title
+	r.Setting = rf.Setting
+	r.Events = sections
+
+	// We should have a completely parsed ReportFull now
 	return r, nil
 }
 
