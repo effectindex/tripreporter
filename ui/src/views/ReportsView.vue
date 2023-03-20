ec51d3a66ea86aba55dcfc024306097739b7e5ef
5HT2 <git@frogg.ie>
2023-03-19 18:09:45 -0700

    ui: (reports view) Impl box

--- ui/src/views/ReportsView.vue
+++ ui/src/views/ReportsView.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="reports">
-    <h1>This is a reports page</h1>
+    <Suspense>
+      <report-box :id="this.$route.query.id ? this.$route.query.id : ''" />
+    </Suspense>
+    <div class="DefaultView__message" id="DefaultView__message">
+      <div class="DefaultView__message_text" id="DefaultView__message_text"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import LayoutDefault from "@/layouts/LayoutDefault.vue";
+import ReportBox from "@/components/ReportBox.vue";
 
 export default {
   name: "ReportsView",
+  components: {
+    ReportBox
+  },
   created() {
     this.$emit('update:layout', LayoutDefault);
   }
