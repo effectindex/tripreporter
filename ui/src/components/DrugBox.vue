aba105a30c3962d006bdbfdb65ee87d9cb6feb40
5HT2 <git@frogg.ie>
2023-03-21 15:08:50 -0700

    ui: (lib) Move drug utils to their own lib

--- ui/src/components/DrugBox.vue
+++ ui/src/components/DrugBox.vue
@@ -8,8 +8,11 @@
 </template>
 
 <script>
+import {getDose, getRoAJoined} from "@/assets/lib/drugs";
+
 export default {
   name: "DrugBox",
+  methods: {getRoAJoined, getDose},
   props: {
     drug: {
       name: String,
@@ -18,72 +21,6 @@ export default {
       roa: Number,
       frequency: Number,
       Prescribed: Number
-    }
-  },
-  methods: {
-    getRoA(drug) {
-      switch (drug.roa) {
-        case 1:
-          return "Other"
-        case 2:
-          return "Oral"
-        case 3:
-          return "Buccal"
-        case 4:
-          return "Rectal"
-        case 5:
-          return "Inhaled"
-        case 6:
-          return "Sublabial"
-        case 7:
-          return "Intranasal"
-        case 8:
-          return "Sublingual"
-        case 9:
-          return "Injection"
-        case 10:
-          return "Buccal Injection"
-        case 11:
-          return "Intravenous Injection"
-        case 12:
-          return "Subcutaneous Injection"
-        case 13:
-          return "Intramuscular Injection"
-        default:
-          return ""
-      }
-    },
-    getRoAJoined(drug) {
-      const roa = this.getRoA(drug);
-      if (roa) {
-        return `, ${roa}`
-      }
-
-      return roa
-    },
-    getPrescribed(drug) {
-      switch (drug.prescribed) {
-        case 1:
-          return "Over the counter"
-        case 2:
-          return "Prescribed by a doctor"
-      }
-    },
-    getDose(drug) {
-      if (drug.dosage === 0 && drug.dosage_unit === "") {
-        return ""
-      }
-
-      if (drug.dosage === 0) {
-        return drug.dosage_unit
-      }
-
-      let joiner = " "
-      if (drug.dosage_unit.length < 5) {
-        joiner = ""
-      }
-
-      return `${drug.dosage}${joiner}${drug.dosage_unit}`
     }
   }
 }
