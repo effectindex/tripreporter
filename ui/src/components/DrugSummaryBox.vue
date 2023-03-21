8e9afb3906247fca52ff496196ae6dfec7814861
5HT2 <git@frogg.ie>
2023-03-21 15:10:18 -0700

    ui: (components) Impl DrugSummaryBox

---
+++ ui/src/components/DrugSummaryBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <header-column-box
+      header="Substances"
+      icon="pills"
+      :columns="['Name', 'Dosage', 'RoA']"
+      :rows="getRows()"
+  />
+</template>
+
+<script>
+import {getDose, getRoA} from "@/assets/lib/drugs";
+import HeaderColumnBox from "@/components/HeaderColumnBox.vue";
+
+export default {
+  name: "DrugSummaryBox",
+  components: {HeaderColumnBox},
+  methods: {
+    getDose, getRoA,
+    getRows() {
+      let rows = []
+      this.events.forEach(e => {
+        if (e.type === 2) {
+          rows.push({'Name': e.drug.name, 'Dosage': getDose(e.drug), 'RoA': getRoA(e.drug)})
+        }
+      })
+      console.log(rows)
+      return rows
+    }
+  },
+  props: {
+    events: undefined
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
