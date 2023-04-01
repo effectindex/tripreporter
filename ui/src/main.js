402c321793af5cb167f323c87bf25d678a6f814f
5HT2 <git@frogg.ie>
2023-04-01 10:54:24 +0200

    ui: Provide `multi-step` from `@formkit/addons`

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,9 +1,11 @@
 import '@formkit/themes/genesis'
 import '@formkit/pro/genesis'
+import '@formkit/addons/css/multistep'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { defaultConfig, plugin } from '@formkit/vue'
 import { createProPlugin, inputs } from '@formkit/pro'
+import { createMultiStepPlugin } from '@formkit/addons'
 import App from './App.vue'
 import router from './router'
 import apiClient from '@/api'
@@ -13,7 +15,7 @@ const pinia = createPinia()
 const app = createApp(App)
     .use(pinia)
     .use(router)
-    .use(plugin, defaultConfig({ theme: 'genesis', plugins: [formkitPro] }))
+    .use(plugin, defaultConfig({ theme: 'genesis', plugins: [formkitPro, createMultiStepPlugin()] }))
     .provide('axios', apiClient)
     .provide('router', router)
 
