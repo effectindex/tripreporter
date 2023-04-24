136fe098a7b6171ac29842aeebdaa4d3bdc0386b
5HT2 <git@frogg.ie>
2023-04-24 04:52:40 +0200

    store: (user) Impl

---
+++ ui/src/assets/lib/userstore.js
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2023 froggie <legal@frogg.ie>
+//
+// SPDX-License-Identifier: OSL-3.0
+
+import { defineStore } from "pinia";
+import log from "@/assets/lib/logger";
+import { ref } from "vue";
+import Timestamp from "@/assets/lib/timestamp";
+import User from "@/assets/lib/user";
+
+export const useUserStore = defineStore('user', {
+  state: () => {
+    return {
+      user: new User({}),
+      createdDate: ref(null),
+      hideMessage: false,
+      apiSuccess: false
+    }
+  },
+  actions: {
+    updateData(status, data) {
+      this.apiSuccess = status === 200;
+
+      if (this.apiSuccess) {
+        log("Loading user data", typeof this.user, typeof data)
+        this.user = new User({
+          id: data.id,
+          display_name: data.display_name,
+          created: data.created
+        });
+        this.createdDate = new Timestamp({ date: data.created, showTime: true, longFormat: true });
+        this.hideMessage = true;
+        log("Loaded user store", typeof this.user, this.user)
+      }
+    },
+    isLoaded() {
+      return this.apiSuccess && this.user !== null
+    }
+  },
+})
