5f653c5fd94fae65943b0570a5f63a1c592d99a0
5HT2 <git@frogg.ie>
2023-04-24 06:15:27 +0200

    lib: (user) Rewrite syntax and nicer handling, add `reports` support

--- ui/src/assets/lib/user.js
+++ ui/src/assets/lib/user.js
@@ -3,13 +3,22 @@
 // SPDX-License-Identifier: OSL-3.0
 
 import Timestamp from "@/assets/lib/timestamp";
+import Report from "@/assets/lib/report";
+import log from "@/assets/lib/logger";
 
 export default class User { // TODO: Rewrite in TS for #106
-  constructor({ obj, id, display_name, created }) {
+  constructor({ id, display_name, created, reports }) {
     this.id = id;
     this.display_name = display_name;
-    this.created = new Timestamp({ date: created, longFormat: true })
-    obj && Object.assign(this, obj);
+    this.reports = [];
+    this.created = new Timestamp({ date: created, longFormat: true });
+
+    for (const n in reports) {
+      let report = new Report(reports[n]);
+      this.reports.push(report)
+    }
+
+    log("user.js", this)
     return this;
   }
 }
