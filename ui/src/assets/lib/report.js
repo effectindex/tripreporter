1f45eebc159d17db6a244e50f6fd125bd9857c8c
5HT2 <git@frogg.ie>
2023-04-24 02:59:45 +0200

    lib: (report) Load subject medications

--- ui/src/assets/lib/report.js
+++ ui/src/assets/lib/report.js
@@ -6,15 +6,26 @@ import ReportEvent from "@/assets/lib/report-event";
 import log from "@/assets/lib/logger";
 
 export default class Report { // TODO: Rewrite in TS for #106
-  constructor({ id, account_id, creation_time, modified_time, report_date, title, setting, report_events }) {
+  constructor({ id, user, account_id, creation_time, modified_time, report_date, title, setting, report_subject, report_events }) {
     this.id = id;
+    this.user = user;
     this.account_id = account_id;
     this.creation_time = creation_time;
     this.modified_time = modified_time;
     this.report_date = report_date;
     this.title = title;
     this.setting = setting;
+    this.medications = [];
+    this.report_subject = report_subject;
     this.report_events = [];
+
+    if (report_subject && report_subject["medications"]) {
+      for (const n in report_subject["medications"]) {
+        let event = new ReportEvent({ obj: report_subject["medications"][n] })
+        this.medications.push(event)
+      }
+    }
+
     for (const n in report_events) {
       let event = new ReportEvent({ obj: report_events[n] })
       this.report_events.push(event)
