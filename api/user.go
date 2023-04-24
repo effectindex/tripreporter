c8749850f5308084143bf3a6deb6f4429fa76242
5HT2 <git@frogg.ie>
2023-04-24 04:52:02 +0200

    api: (user) Impl `/user/{id}`

--- api/user.go
+++ api/user.go
@@ -7,6 +7,8 @@ package api
 import (
 	"net/http"
 
+	"github.com/effectindex/tripreporter/models"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -23,7 +25,21 @@ func UserPost(w http.ResponseWriter, r *http.Request) {
 
 // UserGet path is /api/v1/user/{id}
 func UserGet(w http.ResponseWriter, r *http.Request) {
-	ctx.Handle(w, r, MsgNotImplemented)
+	vars := mux.Vars(r)
+	idStr, _ := vars["id"]
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		ctx.Handle(w, r, MsgNotFound)
+		return
+	}
+
+	user, err := (&models.User{Context: ctx.Context, Unique: models.Unique{ID: id}}).Get()
+	if err != nil {
+		ctx.HandleStatus(w, r, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ctx.HandleJson(w, r, user.CopyPublic(), http.StatusOK)
 }
 
 // UserPatch path is /api/v1/user/{id}
