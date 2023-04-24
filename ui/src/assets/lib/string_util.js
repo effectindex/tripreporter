a12a309e50d3614fbf0bc56fef4ed7e9fc543407
5HT2 <git@frogg.ie>
2023-04-24 03:45:17 +0200

    lib: (string util) Fix missing license header

--- ui/src/assets/lib/string_util.js
+++ ui/src/assets/lib/string_util.js
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2023 froggie <legal@frogg.ie>
+
+SPDX-License-Identifier: OSL-3.0
+-->
+
 export default function titleCase(str) {
   if (str === str.toUpperCase()) {
     return str
