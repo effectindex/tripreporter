8d7166ee971c6187a3a6eab7ea50c329377af3fc
5HT2 <git@frogg.ie>
2023-04-24 03:16:48 +0200

    store: (reports) Impl `reportUser` and `reportSubject`

--- ui/src/assets/lib/reportsstore.js
+++ ui/src/assets/lib/reportsstore.js
@@ -7,12 +7,16 @@ import { defineStore } from "pinia";
 import log from "@/assets/lib/logger";
 import Report from "@/assets/lib/report";
 import Timestamp from "@/assets/lib/timestamp";
+import User from "@/assets/lib/user";
+import ReportSubject from "@/assets/lib/report-subject";
 
 export const useReportsStore = defineStore('reports', {
   state: () => {
     return {
       reportJson: new Report({}),
+      reportUser: ref(null),
       reportDate: ref(null),
+      reportSubject: ref(null),
       hideMessage: false,
       apiSuccess: false
     }
@@ -24,13 +28,19 @@ export const useReportsStore = defineStore('reports', {
       if (this.apiSuccess) {
         log("Loading report data", typeof this.reportJson, typeof data)
         this.reportJson = new Report(data);
-        this.reportDate = new Timestamp({date: this.reportJson.report_date, longFormat: true});
+        this.reportUser = new User({
+          id: this.reportJson.user.id,
+          display_name: this.reportJson.user.display_name,
+          created: this.reportJson.user.created
+        });
+        this.reportDate = new Timestamp({ date: this.reportJson.report_date, longFormat: true });
+        this.reportSubject = new ReportSubject({ obj: this.reportJson.report_subject })
         this.hideMessage = true;
         log("Loaded reports store", typeof this.reportJson)
       }
     },
     isLoaded() {
-      return this.apiSuccess && this.reportJson && this.reportJson !== {}
+      return this.apiSuccess && this.reportJson && this.reportJson !== {} && this.reportUser !== null // subject can be null?
     }
   },
 })
