fad6e1c3e659c9770256d3e3c54a7863e6a230c0
5HT2 <git@frogg.ie>
2023-04-24 12:23:06 +0200

    ui: (subjective report link) Create template

---
+++ ui/src/components/SubjectiveReportLink.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to="/" class="--tr-link-muted">subjective.report</router-link>
+</template>
+
+<script>
+export default {
+  name: "SubjectiveReportLink"
+}
+</script>
+
+<style scoped>
+
+</style>
