b73046e3f3516cadb1b159678abe59cd9f46afb8
5HT2 <git@frogg.ie>
2023-04-24 03:41:14 +0200

    lib: (string util) Impl

---
+++ ui/src/assets/lib/string_util.js
@@ -0,0 +1,12 @@
+export default function titleCase(str) {
+  if (str === str.toUpperCase()) {
+    return str
+  }
+
+  str = str.toLowerCase().split(' ');
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+
+  return str.join(' ');
+}
