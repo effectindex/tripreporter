58e9ada161de47d66dc07d81ca0e046ff71f6c32
5HT2 <git@frogg.ie>
2023-04-28 22:36:10 +0200

    ui: (AuthWrapper) Impl
    See #84

---
+++ ui/src/components/AuthWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <slot v-if="store.activeSession" />
+  <div v-else-if="store.updatedPreviously" v-show="false">
+    {{ router.push('/login') }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthWrapper"
+}
+</script>
+
+<script setup>
+import { inject } from "vue";
+import { useSessionStore } from "@/assets/lib/sessionstore";
+
+const router = inject('router')
+const store = useSessionStore();
+</script>
