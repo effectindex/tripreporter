c5956ef92edd37545e541b0eb0fa3f777101fafa
5HT2 <git@frogg.ie>
2023-04-30 15:53:50 +0200

    db: (patch) Migrate `display_name` column

--- db/patch.go
+++ db/patch.go
@@ -14,34 +14,75 @@ import (
 )
 
 var (
-	patches = []PatchFn{func(c types.Context) bool {
-		db := c.DB()
-		defer db.Commit(context.Background())
+	patches = []PatchFn{
+		func(c types.Context) bool {
+			db := c.DB()
+			defer db.Commit(context.Background())
 
-		var u1 []*models.User
-		if err := pgxscan.Select(context.Background(), db, &u1,
-			`SELECT accounts.id AS id FROM accounts LEFT JOIN users ON accounts.id = users.account_id WHERE users.account_id IS NULL;`); err != nil {
-			c.Logger.Warnw("Patch: Failed to get accounts from DB", zap.Error(err))
-			return false
-		}
+			var u1 []*models.User
+			if err := pgxscan.Select(context.Background(), db, &u1,
+				`SELECT accounts.id AS id FROM accounts LEFT JOIN users ON accounts.id = users.account_id WHERE users.account_id IS NULL;`); err != nil {
+				c.Logger.Warnw("Patch: Failed to get accounts from DB", zap.Error(err))
+				return false
+			}
 
-		confirm := true
-		for _, u := range u1 {
-			if u != nil {
-				u.Context = c
+			confirm := true
+			for _, u := range u1 {
+				if u != nil {
+					u.Context = c
 
-				if u, err := u.Post(); err != nil {
-					confirm = false
-					c.Logger.Warnw("Patch: Failed to create user for account", "user", u, zap.Error(err))
-				} else {
-					c.Logger.Debugw("Patch: Created missing user for account", "user", u)
+					if u, err := u.Post(); err != nil {
+						confirm = false
+						c.Logger.Warnw("Patch: Failed to create user for account", "user", u, zap.Error(err))
+					} else {
+						c.Logger.Debugw("Patch: Created missing user for account", "user", u)
+					}
 				}
 			}
-		}
 
-		c.Logger.Debugw("Patch: Finished patching for missing users")
-		return confirm
-	}}
+			c.Logger.Debugw("Patch: Finished patching for missing users")
+			return confirm
+		},
+		func(c types.Context) bool {
+			db := c.DB()
+			defer db.Commit(context.Background())
+
+			if _, err := db.Exec(context.Background(), `ALTER TABLE accounts ADD COLUMN display_name varchar(255) NOT NULL DEFAULT '';`); err != nil {
+				_ = db.Rollback(context.Background())
+				c.Logger.Warnw("Patch: Failed to update key constraint for accounts_display_name", zap.Error(err))
+				return false
+			}
+
+			c.Logger.Debugw("Patch: Finished patching accounts_display_name")
+			return true
+		},
+		func(c types.Context) bool {
+			db := c.DB()
+			defer db.Commit(context.Background())
+
+			if _, err := db.Exec(context.Background(), `UPDATE accounts SET display_name = users.display_name FROM users WHERE accounts.id = users.account_id;`); err != nil {
+				_ = db.Rollback(context.Background())
+				c.Logger.Warnw("Patch: Failed to copy users.display_name to accounts.display_name", zap.Error(err))
+				return false
+			}
+
+			c.Logger.Debugw("Patch: Finished patching copy users.display_name to accounts.display_name")
+			return true
+		},
+		func(c types.Context) bool {
+			db := c.DB()
+			defer db.Commit(context.Background())
+
+			if _, err := db.Exec(context.Background(), `ALTER TABLE users DROP COLUMN display_name;`); err != nil {
+				_ = db.Rollback(context.Background())
+				c.Logger.Warnw("Patch: Failed to drop column display_name", zap.Error(err))
+				return false
+			}
+
+			c.Logger.Debugw("Patch: Finished patching drop column display_name")
+			return true
+		},
+	}
 )
 
 // PatchFn executes a database patch once
@@ -86,9 +127,7 @@ func SetupPatches(c types.Context) {
 
 	patched := make(map[int]bool, 0)
 	for _, p := range p1 {
-		if p != nil && p.Patched {
-			patched[p.Index] = true
-		}
+		patched[p.Index] = p.Patched
 	}
 
 	for n, p := range patches {
